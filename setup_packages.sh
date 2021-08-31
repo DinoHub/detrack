0ca111e0e7e0391c4bfcdcc34ca79669aa1b3a61
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2021-08-31 21:24:14 +0800

    remove ScaledYOLOv4 submodule

---
+++ setup_packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# install scaledyolov4 and deep_sort_realtime submodules
+cd /media/data/ScaledYOLOv4 && pip3 install -e .
+cd /media/data/deep_sort_realtime && pip3 install -e .
--- setup_submodules.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# install scaledyolov4 and deep_sort_realtime submodules
-cd ScaledYOLOv4 && pip3 install -e .
-cd ../deep_sort_realtime && pip3 install -e .
